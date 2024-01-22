332d794ee81d067e1efb11d1e9b070b3256424e3
MicaeleGomes <micaellyvitoriac@gmail.com>
2024-01-22 09:03:08 -0300

    Define posições

---
+++ funcoes.py
@@ -0,0 +1,15 @@
+def define_posicoes(dados_de_posicionamento):
+    linha = dados_de_posicionamento["linha"]
+    coluna = dados_de_posicionamento["coluna"]
+    orientacao = dados_de_posicionamento["orientacao"]
+    tamanho = dados_de_posicionamento["tamanho"]
+    
+    posicoes = []
+    if orientacao == "vertical":
+        for i in range(tamanho):
+            posicoes.append([linha + i, coluna])
+    elif orientacao == "horizontal":
+        for i in range(tamanho):
+            posicoes.append([linha, coluna + i])
+            
+    return posicoes
