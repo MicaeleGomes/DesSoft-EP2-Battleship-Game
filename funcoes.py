ac14f4b34a1f8a5074f21f2fb1f081fc9f18e1c3
nathanbraga08 <nathancbc@al.insper.edu.br>
2024-01-22 15:19:11 -0300

    função "posição valida" feita

--- funcoes.py
+++ funcoes.py
@@ -48,3 +48,28 @@ def afundados(lista_frota,situacao_atual):
             parte_achada = 0
     return afundado
 
+def posicao_valida(dados_de_posicionamento,lista_frota):
+    #imprimindo a posição desejada pelo jogador
+    posicao_desejada = define_posicoes(dados_de_posicionamento)
+    if lista_frota == []:
+    #acessando a posicao desejada
+        for posicao in posicao_desejada:
+        #verificando se a posicao desejada está no limite
+            if posicao[0] >= 10 or posicao[1] >= 10:
+                return False
+    
+    else: 
+        #acessado a posicao utilizada pela frota
+        for embarcacao in lista_frota:
+            posicao_embarcacao = embarcacao['posicoes']
+            #acessando a posicao desejada
+            for posicao in posicao_desejada:
+                #verificando se a posicao desejada está no limite
+                if posicao[0] >= 10 or posicao[1] >= 10:
+                    return False
+                #verificado se a posicao desejada está sendo usada
+                elif posicao in posicao_embarcacao:
+                    return False
+    #se sair do loop, todos os casos estão disponiveis para uso
+    return True
+            
