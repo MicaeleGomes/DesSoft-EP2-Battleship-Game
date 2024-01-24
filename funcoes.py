23af329a57f72db7385dee9e5b3fefaeef9000e2
MicaeleGomes <micaellyvitoriac@gmail.com>
2024-01-24 11:26:32 -0300

    EP2_Preenche_Frota

--- funcoes.py
+++ funcoes.py
@@ -15,3 +15,15 @@ def define_posicoes(dados_de_posicionamento):
             posicoes.append([linha, coluna + i])
             
     return posicoes
+#EP2: Preenche Frota
+def preenche_frota(dados_de_posicionamento, nome_navio, frota):
+    posicoes_navio = define_posicoes(dados_de_posicionamento)
+    
+    nova_embarcacao = {
+        "tipo": nome_navio,
+        "posicoes": posicoes_navio
+    }
+    
+    frota.append(nova_embarcacao)
+    
+    return frota
