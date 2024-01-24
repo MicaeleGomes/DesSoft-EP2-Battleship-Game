97776c80638f587281c34368dc3b28759f6b5470
MicaeleGomes <micaellyvitoriac@gmail.com>
2024-01-24 11:39:33 -0300

    EP_Preenche_Frota

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,3 @@
-#EP2:Define posições
 
 def define_posicoes(dados_de_posicionamento):
     linha = dados_de_posicionamento["linha"]
@@ -73,3 +72,15 @@ def posicao_valida(dados_de_posicionamento,lista_frota):
     #se sair do loop, todos os casos estão disponiveis para uso
     return True
             
+def preenche_frota(dados_de_posicionamento, nome_navio, frota):
+    posicoes_navio = define_posicoes(dados_de_posicionamento)
+    
+    nova_embarcacao = {
+        "tipo": nome_navio,
+        "posicoes": posicoes_navio
+    }
+    
+    frota.append(nova_embarcacao)
+    
+    return frota
+
